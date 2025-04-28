78949089c20a1ee9364027a64a8374a3a03b0ce2
devopsjose <dummydevopsjose@outlook.com>
2025-04-28 16:00:43 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,12 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstatestorageacct2"
-    container_name        = "tfstate"
-    key                   = "vmss.tfstate"
-    use_msi               = true
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorageacct2"
+    container_name       = "tfstate"
+    key                  = "vmss.tfstate"
+    use_msi              = true
   }
 }
- 
  
  
  
