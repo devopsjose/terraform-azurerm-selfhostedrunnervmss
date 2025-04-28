856ca149b6452d2a44b97c7505df6ca0a16042d6
devopsjose <dummydevopsjose@outlook.com>
2025-04-28 12:44:15 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,14 @@
 terraform {
-   backend "azurerm" {
-     resource_group_name  = "tfstate-rg"
-     storage_account_name = "tfstatestorageacct2"
-     container_name       = "tfstate"
-     key                  = "vmss.tfstate"
--    # remove this line:
--    use_msi              = true
--    # remove this line:
--    use_azuread_auth     = true
- 
-+    # tell Terraform backend to use your Azure CLI / AzureAD login
-+    use_azuread_auth     = true    # ← valid backend flag :contentReference[oaicite:0]{index=0}
-   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatestorageacct2"
+    container_name       = "tfstate"
+    key                  = "vmss.tfstate"
+
+    # tell the backend to use your Azure CLI / OIDC login token
+    use_azuread_auth     = true
+  }
 }
+ 
  
  
