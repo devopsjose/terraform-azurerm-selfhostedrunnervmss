92fc3a0b806d57c6b2ed71c3db9b3b9b0b01df72
devopsjose <dummydevopsjose@outlook.com>
2025-04-28 12:34:25 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,15 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "vmss-test-rg"
+    resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatestorageacct2"
     container_name       = "tfstate"
     key                  = "vmss.tfstate"
-    use_msi              = true
-    use_azuread_auth     = true
+
+-   use_msi          = true
+-   use_azuread_auth = true
++   use_cli          = true    # ← use the Azure CLI login context :contentReference[oaicite:1]{index=1}
++   use_azuread_auth = true
   }
 }
+ 
  
