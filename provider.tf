646328a267e73f6d4baa11daee6467f700eea6cc
devopsjose <dummydevopsjose@outlook.com>
2025-04-29 12:53:58 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-module "vmss" {
-  source                         = "fortytwoservices/selfhostedrunnervmss/azurerm"
-  operating_system               = "ubuntu"       # windows or ubuntu
-  runner_platform                = "azure_devops" # azure_devops or github
-}
