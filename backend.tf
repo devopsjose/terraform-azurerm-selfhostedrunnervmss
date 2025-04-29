867a721500c5ae25c36f41bfd8905ab4f077a000
devopsjose <dummydevopsjose@outlook.com>
2025-04-29 12:52:14 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,7 @@ terraform {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstatestorageacct2"
     container_name       = "tfstate"
-    key                  = "vmss.tfstate"
-    use_msi              = true
+    key                  = "terraform.tfstate"
   }
 }
  
